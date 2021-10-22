4d53ced580ab24261916e402319bd72984d02f17
András Kóré <andras.kore@stud.uni-corvinus.hu>
2021-10-22 20:20:18 +0200

    5.4

--- Value at Risk/Value at Risk/Form1.Designer.cs
+++ Value at Risk/Value at Risk/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Value_at_Risk
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnSave_Click = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -51,11 +52,22 @@ namespace Value_at_Risk
             this.dataGridView2.Size = new System.Drawing.Size(240, 150);
             this.dataGridView2.TabIndex = 1;
             // 
+            // btnSave_Click
+            // 
+            this.btnSave_Click.Location = new System.Drawing.Point(277, 306);
+            this.btnSave_Click.Name = "btnSave_Click";
+            this.btnSave_Click.Size = new System.Drawing.Size(75, 23);
+            this.btnSave_Click.TabIndex = 2;
+            this.btnSave_Click.Text = "button1";
+            this.btnSave_Click.UseVisualStyleBackColor = true;
+            this.btnSave_Click.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave_Click);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -70,6 +82,7 @@ namespace Value_at_Risk
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnSave_Click;
     }
 }
 
--- Value at Risk/Value at Risk/Form1.cs
+++ Value at Risk/Value at Risk/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -70,6 +71,68 @@ namespace Value_at_Risk
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            // Példányosít egyet a windows standard mentés ablakából
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            // Opcionális rész
+            sfd.InitialDirectory = Application.StartupPath; // Alapértelmezetten az exe fájlunk mappája fog megnyílni a dialógus ablakban
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv"; // A kiválasztható fájlformátumokat adjuk meg ezzel a sorral. Jelen esetben csak a csv-t fogadjuk el
+            sfd.DefaultExt = "csv"; // A csv lesz az alapértelmezetten kiválasztott kiterjesztés
+            sfd.AddExtension = true; // Ha ez igaz, akkor hozzáírja a megadott fájlnévhez a kiválasztott kiterjesztést, de érzékeli, ha a felhasználó azt is beírta és nem fogja duplán hozzáírni
+
+            // Ez a sor megnyitja a dialógus ablakot és csak akkor engedi tovább futni a kódot, ha az ablakot az OK gombbal zárták be
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            // Az előző kódsor az alábbi két sor rövidített írásmódja
+            // DialogResult eredmény = sfd.ShowDialog(); // A dialógusablak bezárása után visszakapunk egy DialogResult típusú értéket, mely az ablak bezárásnak körülményeit tárolja
+            // if (eredmény != DialogResult.OK) return; // Ha a bezárás nem az OK gomb lenyomására következett be, akkor kilépünk a metódusból és nem hajtjuk végre a mentést
+
+            // Ha a using blokk használatával példányosítunk egy osztályt akkor a példány csak a using blokk végéig létezik, utána törlésre kerül
+            // StreamWriter és StreamReader használata esetén ezzel a módszerrel megspórolhatjuk a Close() metódus használatát és az írás / olvasási hibák egy részét is lekezeljük
+            // A StreamWriter paraméterei:
+            //    1) Fájlnév: mi itt azt a fájlnevet adjuk át, amit a felhasználó az sfd dialógusban megadott
+            //    2) Append: ha igaz és már létezik ilyen fájl, akkor a végéhez fűzi a sorokat, ha hamis, akkor felülírja a létező fájlt
+            //    3) Karakterkódolás: a magyar nyelvnek is megfelelő legáltalánosabb karakterkódolás az UTF8
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.Write("Időszak;");
+                sw.Write("Nyereség;");
+                // Végigmegyünk a hallgató lista elemein
+                foreach (var nyr in nyereségekRendezve)
+                {
+                    // Egy ciklus iterációban egy sor tartalmát írjuk a fájlba
+                    // A StreamWriter Write metódusa a WriteLine-al szemben nem nyit új sort
+                    // Így darabokból építhetjük fel a csv fájl pontosvesszővel elválasztott sorait
+                    sw.Write(kezdőDátum.ToString(), "-", záróDátum.ToString());
+                    sw.Write(";");
+                    sw.Write(nyr.ToString());
+                    sw.Write(";");
+                    sw.WriteLine(); // Ez a sor az alábbi módon is írható: sr.Write("\n");
+                }
+            }
         }
     }
 }
