70ba09c07628d20c7f4e37423a41f9962e159b75
András Kóré <andras.kore@stud.uni-corvinus.hu>
2021-12-03 22:08:07 +0100

    ÚJ SOLUTION!!! Adatok betöltése

--- week10/Entities/Person.cs
+++ week10/Entities/Person.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Person
+namespace week10.Entities
 {
-    public int BirthYear { get; set; }
-    public Gender Gender { get; set; }
-    public int NbrOfChildren { get; set; }
-    public bool IsAlive { get; set; }
-
-    public Person()
+    public class Person
     {
-        IsAlive = true;
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
     }
 }
