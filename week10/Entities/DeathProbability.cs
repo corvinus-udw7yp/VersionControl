f385bc3655430230ba88773f227788ffc3e89cdc
András Kóré <andras.kore@stud.uni-corvinus.hu>
2021-12-03 23:14:04 +0100

    Felhasználói felület

--- week10/Entities/DeathProbability.cs
+++ week10/Entities/DeathProbability.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using week10.Entities;
 
 namespace week10.Entities
 {
     public class DeathProbability
     {
+        public Gender Gender { get; set; }
         public int Age { get; set; }
-        public int NbrOfChildren { get; set; }
         public double P { get; set; }
 
         public DeathProbability()
