01b04622c462f9dcda88a284038e7a8ccc4c3fbb
András Kóré <andras.kore@stud.uni-corvinus.hu>
2021-12-03 21:43:44 +0100

    Mikroszimuláció

---
+++ week10/Entities/BirthProbability.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class BirthProbability
+{
+    public int Age { get; set; }
+    public int NbrOfChildren { get; set; }
+    public double P { get; set; }
+
+    public BirthProbability()
+    {
+        
+    }
+}
