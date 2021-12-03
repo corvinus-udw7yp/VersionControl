ca440413d7798750a6fe8e14f854ab36b05f9c14
András Kóré <andras.kore@stud.uni-corvinus.hu>
2021-12-03 21:43:44 +0100

    Mikroszimuláció

--- Mikroszimuláció/Form1.cs
+++ Mikroszimuláció/Form1.cs
@@ -8,16 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace week10
+namespace Mikroszimuláció
 {
     public partial class Form1 : Form
     {
-        
         public Form1()
         {
-            List<Person> Population = new List<Person>();
-            List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
-            List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
             InitializeComponent();
         }
     }
---
+++ week10/Entities/DeathProbability.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class DeathProbability
+{
+    public int Age { get; set; }
+    public int NbrOfChildren { get; set; }
+    public double P { get; set; }
+
+    public DeathProbability()
+    {
+        
+    }
+}
